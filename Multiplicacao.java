3586d286236e9e4f247048b58131aec75b77cb22
Juliano Alessandro dos Santos <111141842+julianoAlessandro@users.noreply.github.com>
2023-04-08 21:27:46 -0300

    Alterar arquivo Multiplicação
    Realizei a alteração nos parametros do método

--- Multiplicacao.java
+++ Multiplicacao.java
@@ -1,7 +1,9 @@
 public class Multiplicacao extends OperacaoMatematica{
-	public double calcular(double x, double y) {
-		return   x * y;
+	public double calcular(double num1, double num2) {
+		return   num1 * num2;
 	}
+	
+	
 
 
 }
